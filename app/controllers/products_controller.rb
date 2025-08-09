e0d535acee006b7cf5d41a74545313e2bf6a1b20
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-09 19:17:25 +0200

    Implement Products controller with all CRUD actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,57 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all
   end
 
   def show
   end
 
   def new
+    @product = current_user.products.new
   end
 
   def create
+    @product = current_user.products.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Produit créé avec succès !'
+    else
+      render :new
+    end
   end
 
   def edit
+    unless @product.user == current_user
+      redirect_to products_path, alert: 'Non autorisé'
+    end
   end
 
   def update
+    if @product.user == current_user && @product.update(product_params)
+      redirect_to @product, notice: 'Produit mis à jour !'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @product.user == current_user
+      @product.destroy
+      redirect_to products_path, notice: 'Produit supprimé !'
+    else
+      redirect_to products_path, alert: 'Non autorisé'
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
   end
 end
