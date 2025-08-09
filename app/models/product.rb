045b6e84875c71f6c540e8935636addf55602bce
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-09 11:51:01 +0200

    Add model relationships and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :cart_items, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, length: { maximum: 500 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
